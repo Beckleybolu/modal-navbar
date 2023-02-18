30167dcea6ba496cf6556b75ed38a4ae5f189015
Bolu Beckley <beckleybolu@yahoo.com>
2023-02-18 16:09:48 +0100

    modalsidebar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./home";
+import Modal from "./modal";
+import Sidebar from "./sidebar";
+function App() {
+  return (
+    <>
+    <Home/>
+    <Modal/>
+    <Sidebar/>
+    </>
+  );
+}
+
+export default App;
